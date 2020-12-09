2f1da6285e48b3ea7774fb2590f17f7d40939306
Bendi11 <bkliebmann@gmail.com>
2020-12-08 16:37:59 -0800

    Added assets

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,11 @@
 Chess by Benjamin L (Bendi11)
 Chess game made in SDL2
 Compile commands: 
-Windows: g++ -o bin/Chess.exe src/main.cpp src/init.cpp src/input.cpp src/fileLoader.cpp src/board.cpp -Isrc/include -lSDL2main -lSDL2 -lSDL2_image -static-libgcc
+Windows: 
+g++ -o bin/Chess.exe src/main.cpp src/init.cpp src/input.cpp src/fileLoader.cpp src/board.cpp src/render.cpp -Isrc/include -lSDL2main -lSDL2 -lSDL2_image -static-libgcc
 
-Linux: g++ -o bin/Chess src/main.cpp src/init.cpp src/input.cpp src/fileLoader.cpp src/board.cpp src/render.cpp -Isrc/include -lSDL2main -lSDL2 -lSDL2_image -static-libgcc
+Linux: 
+g++ -o bin/Chess src/main.cpp src/init.cpp src/input.cpp src/fileLoader.cpp src/board.cpp src/render.cpp -Isrc/include -lSDL2main -lSDL2 -lSDL2_image -static-libgcc
 */
 #include "include/main.hpp"
 
@@ -16,7 +18,14 @@ int main(int argc, char** argv)
     SDL_Rect pos;
     pos.x = 0;
     pos.y = 0;
-    SDL_RenderCopy(g.render, g.Board.wKing.icon, NULL, &pos);
+    pos.w = 188;
+    pos.h = 188;
+    SDL_Rect Pos;
+    Pos.x = 0;
+    Pos.y = 0;
+    Pos.w = pos.w * 0.47872340425;
+    Pos.h = pos.h * 0.47872340425;
+    SDL_RenderCopy(g.render, g.Board.wKing.icon, &pos, &Pos);
     while(g.IS_RUNNING)
     {
         g.renderUpdate();
