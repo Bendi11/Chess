d44bfa04b70d9ffd922fe40b4c944cf3b3ee57e7
Bendi11 <bkliebmann@gmail.com>
2020-12-09 22:11:35 -0800

    Added icon support for exe

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,10 @@ Chess by Benjamin L (Bendi11)
 Chess game made in SDL2
 Compile commands: 
 Windows: 
-g++ -o bin/Chess.exe src/main.cpp src/Chess.cpp src/render.cpp -Isrc/include -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -static-libgcc -mwindows
+g++ -o bin/Chess.exe src/main.cpp src/Chess.cpp src/render.cpp -Isrc/include -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -static-libgcc -mwindows icon.o
 
 Linux: 
-g++ -o bin/Chess src/main.cpp src/Chess.cpp src/render.cpp -Isrc/include -lSDL2main -lSDL2 -lSDL2_image -static-libgcc
+g++ -o bin/Chess src/main.cpp src/Chess.cpp src/render.cpp -Isrc/include -lSDL2main -lSDL2 -lSDL2_image -static-libgcc icon.o
 */
 #include "include/Chess.hpp"
 #include "include/render.hpp"
