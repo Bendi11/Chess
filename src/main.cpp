81ed2ef3f48ebd9a1ca1f51628039871108dd8fc
Bendi11 <bkliebmann@gmail.com>
2020-12-10 19:57:05 -0800

    Attempted to fix netcode

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ Linux:
 g++ -o bin/Chess src/main.cpp src/Chess.cpp src/render.cpp -Isrc/include -lSDL2main -lSDL2 -lSDL2_image -static-libgcc -static-libstdc++ icon.o
 */
 
-#define PORT 23
+#define PORT 8080
 #include "include/Chess.hpp"
 #include "include/render.hpp"
 
@@ -23,7 +23,8 @@ int socketID; //Socket ID
 int otherSocket; //Client socket ID
 struct sockaddr_in sockAddress; //The address of the socket
 int addrlen = sizeof(sockAddress); //How long the address is
-char recievedStr[1024]; //Recieved string
+std::string receivedStr; //Recieved string
+std::string sentStr; //Sent string
 
 
 //Function to show a message box asking for client / server socket connection
@@ -77,9 +78,9 @@ void showStartupBox()
     }
     else if(buttonID == 1) //User decided to host a game
     {
-        socketID = socket(AF_INET, SOCK_STREAM, 0); //Create a new socket
+        /*socketID = socket(AF_INET, SOCK_STREAM, 0); //Create a new socket
         int opt = 1;
-        if(setsockopt(socketID, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
+        setsockopt(socketID, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
 
         sockAddress.sin_family = AF_INET; //IPv4
         sockAddress.sin_addr.s_addr = INADDR_ANY; //Accept any connection
@@ -88,12 +89,63 @@ void showStartupBox()
         bind(socketID, (struct sockaddr *)&sockAddress, sizeof(sockAddress)); //Bind the socket to the right port
         listen(socketID, 2); //Start listening on the socket
         otherSocket = accept(socketID, (struct sockaddr *)&sockAddress, (socklen_t *)&addrlen); //Await a connection request
+        return;*/
 
+        int server_fd, new_socket, valread; 
+    struct sockaddr_in address; 
+    int opt = 1; 
+    int addrlen = sizeof(address); 
+    char buffer[1024] = {0}; 
+    char *hello = "Hello from server"; 
+       
+    // Creating socket file descriptor 
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
+    { 
+        perror("socket failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+       
+    // Forcefully attaching socket to the port 8080 
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
+                                                  &opt, sizeof(opt))) 
+    { 
+        perror("setsockopt"); 
+        exit(EXIT_FAILURE); 
+    } 
+    address.sin_family = AF_INET; 
+    address.sin_addr.s_addr = INADDR_ANY; 
+    address.sin_port = htons( PORT ); 
+       
+    // Forcefully attaching socket to the port 8080 
+    if (bind(server_fd, (struct sockaddr *)&address,  
+                                 sizeof(address))<0) 
+    { 
+        perror("bind failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+    if (listen(server_fd, 3) < 0) 
+    { 
+        perror("listen"); 
+        exit(EXIT_FAILURE); 
+    } 
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,  
+                       (socklen_t*)&addrlen))<0) 
+    { 
+        perror("accept"); 
+        exit(EXIT_FAILURE); 
+    } 
+    send(new_socket , hello , strlen(hello) , 0 ); 
+
+    return;
     }
 
 } 
 
+//Function to receive a move from the socket and
+void parseSentMove()
+{
 
+}
 
 int main(int argc, char** argv)
 {
