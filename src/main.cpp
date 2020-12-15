0ca9d62215d97884b69ca226e49496a95094693e
Bendi11 <bkliebmann@gmail.com>
2020-12-14 17:38:52 -0800

    Added option to play vs computer

--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,7 @@ std::string storedWMove = " ";
 tcp::acceptor servAccept(ioContext, tcp::endpoint(tcp::v4(), 8080)); //Server request acceptor   
 
 bool server = true; //If the user is hosting
+bool isOnline = true; //If the player is playing on LAN
 
 //Function to be run in parallel with the game logic to check for messages being sent
 static void netThread(Chess::board_t& Board, bool server, tcp::socket& socket)
@@ -77,7 +78,8 @@ void showStartupBox()
     const SDL_MessageBoxButtonData buttons[] = 
     {
         {/*Flags,   buttonid, text*/ 0, 0, "Join a game"},
-        {0, 1, "Host a game"}
+        {0, 1, "Host a game"},
+        {0, 2, "Play vs computer"}
     };
     const SDL_MessageBoxData messageData = 
     {
@@ -99,6 +101,11 @@ void showStartupBox()
     {
         server = true;
     }
+    else if(buttonID == 2)
+    {
+        isOnline = false;
+        server = true;
+    }
 
 } 
 
@@ -112,52 +119,60 @@ int main(int argc, char** argv)
 
     showStartupBox();
 
-    tcp::socket socket(ioContext); //Make a new socket
-    if(server)
+    if(isOnline)
     {
-        //Get IP of localhost eg. the host's IP
-        std::string localIPs = "Type this number on the other computer: ";
-        tcp::resolver resolver(ioContext); //Resolver to resolve IP from name
-        tcp::resolver::query query(asio::ip::host_name(), ""); //Query for the resolver
-        tcp::resolver::iterator iter = resolver.resolve(query); //Resolve the query
-        tcp::resolver::iterator end; // End marker.
-        while (iter != end) //Iterate through the list of IPs received from query
+        tcp::socket socket(ioContext); //Make a new socket
+        if(server)
         {
-            tcp::endpoint ep = *iter++; 
-            if(ep.address().is_v4()) //If the IP is not an IPv6...
+            //Get IP of localhost eg. the host's IP
+            std::string localIPs = "Type this number on the other computer: ";
+            tcp::resolver resolver(ioContext); //Resolver to resolve IP from name
+            tcp::resolver::query query(asio::ip::host_name(), ""); //Query for the resolver
+            tcp::resolver::iterator iter = resolver.resolve(query); //Resolve the query
+            tcp::resolver::iterator end; // End marker.
+            while (iter != end) //Iterate through the list of IPs received from query
             {
-                localIPs.append(ep.address().to_string()); //Add it to the message box
-                localIPs.append(", ");
+                tcp::endpoint ep = *iter++; 
+                if(ep.address().is_v4()) //If the IP is not an IPv6...
+                {
+                    localIPs.append(ep.address().to_string()); //Add it to the message box
+                    localIPs.append(", ");
+                }
             }
+            SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "Chess Hosting", localIPs.c_str(), NULL); //Show the user what to paste to the other computer
+            servAccept.accept(socket); //Await a connection request
+        } 
+        else 
+        {
+            std::string ip = d.getTextInput(); //Prompt the user to enter an IP address
+            tcp::endpoint ep(asio::ip::address::from_string(ip), 8080); //Convert to an IP and attempt a connection
+            socket.connect(ep);
         }
-        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "Chess Hosting", localIPs.c_str(), NULL); //Show the user what to paste to the other computer
-        servAccept.accept(socket); //Await a connection request
-    } 
-    else 
-    {
-        std::string ip = d.getTextInput(); //Prompt the user to enter an IP address
-        tcp::endpoint ep(asio::ip::address::from_string(ip), 8080); //Convert to an IP and attempt a connection
-        socket.connect(ep);
+        std::thread netThread1(netThread, std::ref(b), server, std::ref(socket)); //Start the receiving network thread
     }
-
+    
     d.WHITEORBLACK = server; //Set the player's color based on server and host
-    std::thread netThread1(netThread, std::ref(b), server, std::ref(socket)); //Start the receiving network thread
+    
 
     while(d.running)
     {
         d.drawBoard(b);
         d.input(b);
         
-        if(storedBMove != b.bMoveString && !server)
+        if(isOnline)
         {
-            storedBMove = b.bMoveString;
-            asio::write(socket, asio::buffer(b.bMoveString));
+            if(storedBMove != b.bMoveString && !server)
+            {
+                storedBMove = b.bMoveString;
+                asio::write(socket, asio::buffer(b.bMoveString));
+            }
+            else if(storedWMove != b.wMoveString && server) //Check if we need to send a move and we are white
+            {
+                storedWMove = b.wMoveString;
+                asio::write(socket, asio::buffer(b.wMoveString));
+            }
         }
-        else if(storedWMove != b.wMoveString && server) //Check if we need to send a move and we are white
-        {
-            storedWMove = b.wMoveString;
-            asio::write(socket, asio::buffer(b.wMoveString));
-        }
+        
 
         if(b.WINNER != WINNER_NONE)
         {
