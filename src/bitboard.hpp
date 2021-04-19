49fbee3a62b6be4014e26b68e3e87389f24a304b
Bendi11 <bkliebmann@gmail.com>
2021-04-19 15:59:55 -0700

    Added magics

--- src/bitboard.hpp
+++ src/bitboard.hpp
@@ -3,6 +3,22 @@
 #include <string>
 #include <type_traits>
 
+
+
+/**
+ * @brief Nasty template function to convert an enum to its underlying type
+ * 
+ * @tparam T The type of enum for the template function
+ * @tparam std::is_enum<T>::type SFINAE to make sure that the template can only be created for an enum
+ * @param e The enum to convert to the underlying type
+ * @return std::underlying_type<T>::type The underlying type representing the enumeration
+ */
+template<class T, class U = typename std::is_enum<T>::type >
+constexpr inline typename std::underlying_type<T>::type rawenum(T e) {
+    return static_cast< std::underlying_type<T>::type>(e);
+}
+
+
 /**
  * @brief bitboard is a simple typedef for uint64_t, it is a raw number with no
  * extra methods or functions to help with bitwise operations 
@@ -113,7 +129,6 @@ public:
         return Bb(this->m_bb ^ static_cast<uint64_t>(other));
     }
 
-
     /// @brief Get the nth bit of the bitboard
     inline constexpr bool nth(uint8_t n) const {return (m_bb >> n) & 1;}
 
@@ -136,6 +151,18 @@ public:
             ((rev >> 16) & 0xffff0000) | (rev >> 48);
     }
 
+    /// @brief Reverse a bitboard from an existing bitboard
+    static Bb reverse(bitboard bits) 
+    {
+        bitboard rev = bits;
+        rev = (rev & 0x5555555555555555) << 1 | (rev >> 1) & 0x5555555555555555;
+        rev = (rev & 0x3333333333333333) << 2 | (rev >> 2) & 0x3333333333333333;
+        rev = (rev & 0x0f0f0f0f0f0f0f0f) << 4 | (rev >> 4) & 0x0f0f0f0f0f0f0f0f;
+        rev = (rev & 0x00ff00ff00ff00ff) << 8 | (rev >> 8) & 0x00ff00ff00ff00ff;
+
+        return (rev << 48) | ((rev & 0xffff0000) << 16) |
+            ((rev >> 16) & 0xffff0000) | (rev >> 48);
+    }
 private:
     /// @brief Internal raw bitboard that member methods operate on, this is the *ONLY* data that Bb contains, so the size of Bb
     /// is always == the size of bitboard == 8 bytes or 64 bits
@@ -175,6 +202,8 @@ public:
         a8, b8, c8, d8, e8, f8, g8, h8
     };
 
+    constexpr operator uint8_t() {return rawenum(this->val);}
+
     /**
      * @brief Overloaded constructor so that creating a BbSquare struct from an Sq enum is possible
      * 
@@ -189,13 +218,44 @@ public:
 
     //Compare two bitboard squares and check for equality
     constexpr bool operator==(const BbSquare& other) const {return this->val == other.val;}
+    constexpr bool operator!=(const BbSquare::Sq& other) const {return this->val != other;}
     constexpr bool operator!=(const  BbSquare& other) const {return this->val != other.val;}
+
+    BbSquare& operator++() {this->val = static_cast<Sq>(rawenum(this->val) + 1); return *this;}
+
+    /// @brief Get the file of this square
+    inline uint8_t file(void)
+    {
+        return static_cast<uint8_t>(rawenum(val) & 7);
+    }
+
+    /// @brief Get the rank index of this square
+    inline uint8_t rank(void)
+    {
+        return static_cast<uint8_t>(rawenum(val) >> 3);
+    }
+
+    /// @brief Get the diagonal of this square
+    inline uint8_t diagonal(void)
+    {
+        return 7 + rank() - file();
+    }
+
+    /// @brief Get the anti diagonal of this square
+    inline uint8_t antidiagonal(void)
+    {
+        return rank() + file();
+    }
 
     operator bool() = delete;
     
 private:
     Sq val; //The value of the bitboard square
 };
+
+
+extern bitboard BISHOP_ATTACKS[64][512];
+extern constexpr bitboard get_bishop_attacks(BbSquare square, bitboard occ);
 
 #define REPEAT_8(bb) 0b##bb##bb##bb##bb##bb##bb##bb##bb
 /// @brief Bitboard masks of each file on a chess board, 0 through 7
@@ -267,17 +327,25 @@ constexpr inline bitboard ANTIDIAG_MASKS[15]
 
 
 /**
- * @brief Nasty template function to convert an enum to its underlying type
+ * @brief Generate sliding piece attacks for a given mask of squares to travel, occupancy bitboard,
+ * and square to move from
  * 
- * @tparam T The type of enum for the template function
- * @tparam std::is_enum<T>::type SFINAE to make sure that the template can only be created for an enum
- * @param e The enum to convert to the underlying type
- * @return std::underlying_type<T>::type The underlying type representing the enumeration
+ * @param sq The square to move from
+ * @param occupancy The occupancy bitboard for enemy pieces
+ * @param mask The mask of squares to move
+ * @return Bb The bitboard that represents valid attacks
  */
-template<class T, class U = typename std::is_enum<T>::type >
-constexpr inline typename std::underlying_type<T>::type rawenum(T e) {
-    return static_cast< std::underlying_type<T>::type>(e);
+inline Bb slide_attacks(BbSquare sq, Bb occupancy, Bb mask)
+{
+    return (((mask & occupancy) - static_cast<Bb>(sq) * 2) ^ Bb::reverse((mask & occupancy).reverse() - (static_cast<Bb>(sq).reverse() * 2) )) & mask;
 }
+
+inline Bb bishop_attacks(BbSquare sq, Bb occ)
+{
+    return slide_attacks(sq, occ, DIAG_MASKS[sq.diagonal()]) | slide_attacks(sq, occ, ANTIDIAG_MASKS[sq.antidiagonal()]);
+}
+
+void init_bishop();
 
 /**
  * @brief Structure with information about a direction on a compass rose,
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
 #include "bitboard.hpp"
 #include <iostream>
 
+
+
 int main(int argc, char* argv[])
 {
-    Bb b = RANK_MASKS[3];
+    init_bishop();
+    Bb b = 0;
+    b.set(BbSquare::c3, true);
+    b.set(BbSquare::g7, true);
     //b |= 1 << BbSquare::a1;
     //b.set(BbSquare::a4, true);
-    std::cout << b.str() << std::endl;
+    std::cout << Bb(get_bishop_attacks(BbSquare::g7, b) ).str() << "\n Occ:\n" << b.str() << std::endl;
+    char c;
+    std::cin >> c;
     return 0;
 }
